ea3f07ce9a8f48355db92f61aad88b7fafe280a0
hyunkk1 <bbang970723@naver.com>
2024-03-03 01:25:40 +0900

    Java Practice 0303

---
+++ src/버스문제_3월3일연습/bus_0303.java
@@ -0,0 +1,27 @@
+package 버스문제_3월3일연습;
+
+import java.util.Scanner;
+
+public class bus_0303 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("버스 정보를 입력하세요 : ");
+
+        String bus = sc.nextLine();
+
+        if (bus.equals("상암08")) {
+            System.out.println(bus + "번 버스");
+            System.out.println("약 3분 후 도착");
+            System.out.println("남은거리 1.2km");
+        } else if (bus.equals("1234")) {
+            System.out.println(bus + "번 버스");
+            System.out.println("약 5분 후 도착");
+            System.out.println("남은거리 0.8km");
+        } else {
+            System.out.println("버스번호를 다시 입력하세요");
+        }
+    }
+
+}
+
+
